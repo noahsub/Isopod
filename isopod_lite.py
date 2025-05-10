62e97e43a1b2a2816262a783c28bb01790211c75
noahsub <48663769+noahsub@users.noreply.github.com>
2025-05-10 18:03:03 -0400

    Feature: Implemented isopod_lite.py which allows for the execution of podman commands for an isolated container

---
+++ isopod_lite.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import shlex
+
+from Managers.file_manager import create_directory, delete_directory, directory_exists
+from Managers.system_manager import has_permission, run_command
+
+if __name__ == "__main__":
+    location = Path(input('Container Directory: '))
+
+    # If the directory has not been created yet
+    if not directory_exists(location):
+        # Check if the parent directory exists and the user has permissions for it
+        if not (directory_exists(location.parent) and has_permission(location.parent)):
+            # Either the parent directory does not exist or the user does not have permissions, hence return
+            print(f"Either the parent directory does not exist or the user does not have permissions for {location.parent}")
+            exit(1)
+        else:
+            # Create the directory
+            create_directory(location)
+            create_directory(location.joinpath('data'))
+            create_directory(location.joinpath('data', 'storage'))
+            create_directory(location.joinpath('data', 'run'))
+            create_directory(location.joinpath('data', 'tmp'))
+            create_directory(location.joinpath('data', 'xdg'))
+
+    environment_variables = [('XDG_RUNTIME_DIR', str(location.joinpath('data', 'xdg')))]
+
+    podman_base_command = [
+        'podman', 
+        '--root', str(location.joinpath('data', 'storage')), 
+        '--runroot', str(location.joinpath('data', 'run')), 
+        '--tmpdir', str(location.joinpath('data', 'tmp')), 
+        '--storage-opt', 'mount_program=/usr/bin/fuse-overlayfs'
+    ]
+
+    # Check for reboot or errors
+
+    if run_command(podman_base_command + ['ps', '-a'], capture_output=False, text=False).returncode != 0:
+        print('Error detected, cleaning up...')
+        delete_directory(location.joinpath('data', 'run'))
+        delete_directory(location.joinpath('data', 'tmp'))
+        delete_directory(location.joinpath('data', 'xdg', 'libpod', 'tmp'))
+    
+    while True:
+        # Get input
+        command = input('Podman Command (without podman): ')
+
+        # Split the command safely
+        cmd = podman_base_command + shlex.split(command)
+
+        output = run_command(cmd, capture_output=True, text=True, env=environment_variables)
+        print(f'Return Code: {output.returncode}')
+        print(f'Ouput:')
+        print(output.stdout)
+        print(f'Error:')
+        print(output.stderr)
